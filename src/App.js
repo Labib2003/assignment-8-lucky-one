b5c870c1a776d3b650b42067394200fe73055ea7
Labib2003 <labib.salimi30@gmail.com>
2022-03-25 22:46:36 +0600

    fake data created and tested

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("data.json")
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, [])
+
   return (
     <div className="App">
-      
+      <header>
+        <h3>Toy store</h3>
+      </header>
     </div>
   );
 }
